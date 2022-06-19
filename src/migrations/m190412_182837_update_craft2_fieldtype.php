d8cab464abbe05f9ae90d58816eda47acc1180f3
Josh Crawford <joshua@sgroup.com.au>
2022-06-19 21:43:50 +1000

    Migration

--- src/migrations/m190412_182837_update_craft2_fieldtype.php
+++ src/migrations/m190412_182837_update_craft2_fieldtype.php
@@ -3,7 +3,9 @@ namespace verbb\tablemaker\migrations;
 
 use verbb\tablemaker\fields\TableMakerField;
 
+use Craft;
 use craft\db\Migration;
+use craft\services\Plugins;
 
 class m190412_182837_update_craft2_fieldtype extends Migration
 {
@@ -14,6 +16,20 @@ class m190412_182837_update_craft2_fieldtype extends Migration
         $this->update('{{%fields}}', [
             'type' => TableMakerField::class
         ], ['type' => 'TableMaker']);
+
+        // For some reason from a Craft 2 site, the field settings have ended up on the plugin settings and this causes
+        // an application warning log message to be raised as this plugin has no defined plugin settings model
+        $projectConfig = Craft::$app->getProjectConfig();
+        $plugins = $projectConfig->get(Plugins::CONFIG_PLUGINS_KEY) ?? [];
+
+        if (isset($plugins['tablemaker']['settings'])) {
+            $projectConfig->muteEvents = true;
+
+            echo "    > Removing plugin settings key...\n";
+            $projectConfig->remove('plugins.tablemaker.settings');
+
+            $projectConfig->muteEvents = false;
+        }
 
         return true;
     }
