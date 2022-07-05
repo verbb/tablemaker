0df1fea3583099044497c1f698fdcf9bdc921d09
Josh Crawford <joshua@sgroup.com.au>
2022-07-06 09:52:49 +1000

    Fix an error when running the Craft 2 migration on an already Craft 3 updated install

--- src/migrations/m190412_182837_update_craft2_fieldtype.php
+++ src/migrations/m190412_182837_update_craft2_fieldtype.php
@@ -5,7 +5,6 @@ use verbb\tablemaker\fields\TableMakerField;
 
 use Craft;
 use craft\db\Migration;
-use craft\services\Plugins;
 
 class m190412_182837_update_craft2_fieldtype extends Migration
 {
@@ -14,25 +13,7 @@ class m190412_182837_update_craft2_fieldtype extends Migration
 
     public function safeUp(): bool
     {
-        echo "    > Updating Table Maker field type...\n";
-
-        $this->update('{{%fields}}', [
-            'type' => TableMakerField::class,
-        ], ['type' => 'TableMaker']);
-
-        // For some reason from a Craft 2 site, the field settings have ended up on the plugin settings and this causes
-        // an application warning log message to be raised as this plugin has no defined plugin settings model
-        $projectConfig = Craft::$app->getProjectConfig();
-        $plugins = $projectConfig->get(Plugins::CONFIG_PLUGINS_KEY) ?? [];
-
-        if (isset($plugins['tablemaker']['settings'])) {
-            $projectConfig->muteEvents = true;
-
-            echo "    > Removing plugin settings key...\n";
-            $projectConfig->remove('plugins.tablemaker.settings');
-
-            $projectConfig->muteEvents = false;
-        }
+        $this->update('{{%fields}}', ['type' => TableMakerField::class], ['type' => 'TableMaker']);
 
         return true;
     }
